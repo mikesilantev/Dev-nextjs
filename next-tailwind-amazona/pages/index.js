6da3601ce9f98008acfed85ee8004a5ba2812e24
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-12-23 00:27:20 +0300

    132

---
+++ next-tailwind-amazona/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hallo world!</title>
+      </Head>
+
+      <h1 className='text-3xl font-bold'>hello world</h1>
+      <div className=''></div>
+    </div>
+  );
+}
